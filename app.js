395f0362e241cfb1981e070d0beecd538bcf91c1
mafeep <123569644+mafeep@users.noreply.github.com>
2023-02-06 12:02:56 -0500

    avance app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "1",
+      img: "image/1.png"
+    },
+    {
+      name: "2",
+      img: "image/2.png"
+    },
+    {
+      name: "3",
+      img: "image/3.png"
+    },
+    {
+      name: "4",
+      img: "image/4.png"
+    },
+    {
+      name: "5",
+      img: "image/5.png"
+    },
+    {
+      name: "6",
+      img: "image/6.png"
+    },
+    {
+      name: "1",
+      img: "image/1.png"
+    },
+    {
+      name: "2",
+      img: "image/2.png"
+    },
+    {
+      name: "3",
+      img: "image/3.png"
+    },
+    {
+      name: "4",
+      img: "image/4.png"
+    },
+    {
+      name: "5",
+      img: "image/5.png"
+    },
+    {
+      name: "6",
+      img: "image/6.png"
+    }
+  ];
+});
